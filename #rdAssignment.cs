298eb021b72f21703c740bdc0e0dc82aa4224902
JorelDenceReyethJMorales <164605087+JorelDenceReyethJMorales@users.noreply.github.com>
2024-03-23 23:36:28 +0800

    Add files via upload
    3rd Assignment 
    O O P

---
+++ #rdAssignment.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using System.Runtime.InteropServices;
+using System.Timers;
+
+namespace _3rdAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input Seconds :  ");
+            string a = Console.ReadLine();
+            float A = (int)Int64.Parse(a);
+            
+            float StM = A   / 60;
+            float MtH = StM / 60;
+            float HtD = MtH / 24;
+            float DtM = HtD / 30;
+
+            Console.WriteLine("Seconds to Minutes :" + StM + " Minutes");
+            Console.WriteLine("Minutes to Hours   :" + MtH + " Hours");
+            Console.WriteLine("Hours   to Days    :" + HtD + " Days");
+            Console.WriteLine("Days    to Months  :" + DtM + " Months");
+
+        }
+    }
+}
