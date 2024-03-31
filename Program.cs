3abd0bd781094bc1a16b58387d49f8a806bbc421
JorelDenceReyethJMorales <164605087+JorelDenceReyethJMorales@users.noreply.github.com>
2024-03-31 10:37:21 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ASSIGNMENT_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input Seconds :  ");
+            string a = Console.ReadLine();
+            float A = (int)Int64.Parse(a);
+
+            float StM = A / 60;
+            float MtH = StM / 60;
+            float HtD = MtH / 24;
+            float DtM = HtD / 30;
+
+            Console.WriteLine("Seconds to Minutes :" + StM + " Minutes");
+            Console.WriteLine("Minutes to Hours   :" + MtH + " Hours");
+            Console.WriteLine("Hours   to Days    :" + HtD + " Days");
+            Console.WriteLine("Days    to Months  :" + DtM + " Months");
+        }
+    }
+}
